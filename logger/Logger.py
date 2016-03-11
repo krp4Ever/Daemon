bf02eb12c795a81f3978f1dd05f166dc9d79c004
pankairan <pankairan@baidu.com>
2016-03-11 18:22:44 +0800

    modify logger

--- logger/Logger.py
+++ logger/Logger.py
@@ -2,13 +2,13 @@ import logging
 import logging.config
 
 class Logger:
-    fileLogger = None
+    daemonLogger = None
     consoleLogger = None
 
     @staticmethod
     def initlogger(confFile):
 	    logging.config.fileConfig(confFile)
-	    Logger.fileLogger = logging.getLogger("daemon")
+	    Logger.daemonLogger = logging.getLogger("daemon")
 	    Logger.consoleLogger = logging.getLogger("console")
 
     @staticmethod
@@ -18,9 +18,9 @@ class Logger:
     @staticmethod
     def warning(msg):
         Logger.consoleLogger.warning(msg)
-        Logger.fileLogger.warning(msg)
+        Logger.daemonLogger.warning(msg)
 
     @staticmethod
     def error(msg):
         Logger.consoleLogger.error(msg)
-        Logger.fileLogger.error(msg)
+        Logger.daemonLogger.error(msg)
